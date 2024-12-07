7b695ea9e1e1ec9fcceafa382f988f3d89312669
Gurjot Singh Aulakh <gurjot.singh.aulakh28@gmail.com>
2024-12-07 19:13:11 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,24 +14,9 @@ const outfit = Outfit({ subsets: ["latin"] });
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <iframe
-      src="https://www.chatbase.co/chatbot-iframe/FCdzkVvZdDuGnoykHddUO"
-      width="100%"
-      style="height: 100%; min-height: 700px"
-      frameborder="0"
-      ></iframe>
-      <script>
-      window.embeddedChatbotConfig = {
-      chatbotId: "FCdzkVvZdDuGnoykHddUO",
-      domain: "www.chatbase.co"
-      }
-      </script>
-      <script
-      src="https://www.chatbase.co/embed.min.js"
-      chatbotId="FCdzkVvZdDuGnoykHddUO"
-      domain="www.chatbase.co"
-      defer>
-      </script>
+      <iframe src="https://www.chatbase.co/chatbot-iframe/FCdzkVvZdDuGnoykHddUO" width="100%" style="height: 100%; min-height: 700px" frameborder="0" ></iframe>
+      <script> window.embeddedChatbotConfig = { chatbotId: "FCdzkVvZdDuGnoykHddUO", domain: "www.chatbase.co"} </script>
+      <script src="https://www.chatbase.co/embed.min.js" chatbotId="FCdzkVvZdDuGnoykHddUO" domain="www.chatbase.co" defer></script>
       <body className={outfit.className}>
       <link rel="icon" href="/logo-light.png" sizes="any" />
         <ThemeProvider attribute="class" defaultTheme="light">
