d8388a30b47073aef94a20275de986e88b32ef73
Gurjot Singh Aulakh <gurjot.singh.aulakh28@gmail.com>
2024-12-07 19:13:49 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,43 @@
 import { Outfit } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
-// components
+// Components
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+
 // ThemeProvider
 import { ThemeProvider } from "@/components/ThemeProvider";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <iframe src="https://www.chatbase.co/chatbot-iframe/FCdzkVvZdDuGnoykHddUO" width="100%" style="height: 100%; min-height: 700px" frameborder="0" ></iframe>
-      <script> window.embeddedChatbotConfig = { chatbotId: "FCdzkVvZdDuGnoykHddUO", domain: "www.chatbase.co"} </script>
-      <script src="https://www.chatbase.co/embed.min.js" chatbotId="FCdzkVvZdDuGnoykHddUO" domain="www.chatbase.co" defer></script>
+      <head>
+        <link rel="icon" href="/logo-light.png" sizes="any" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.embeddedChatbotConfig = {
+                chatbotId: "FCdzkVvZdDuGnoykHddUO",
+                domain: "www.chatbase.co"
+              };
+            `,
+          }}
+        />
+        <script
+          src="https://www.chatbase.co/embed.min.js"
+          data-chatbot-id="FCdzkVvZdDuGnoykHddUO"
+          data-domain="www.chatbase.co"
+          defer
+        ></script>
+      </head>
       <body className={outfit.className}>
-      <link rel="icon" href="/logo-light.png" sizes="any" />
         <ThemeProvider attribute="class" defaultTheme="light">
           <Header />
-          {children}
+          <main>{children}</main>
           <Analytics />
           <SpeedInsights />
           <Footer />
